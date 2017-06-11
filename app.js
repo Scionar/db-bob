f29806be0da0d763d88c7169d6f77ecd550a71f1
Joona Viertola <contact@joonaviertola.com>
2017-06-11 21:50:27 +0300

    Starter parsing JSON and doing query strings. Pg node module added.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Meow = require("./meow.js");
 
-let db = new Meow('localhost', '3322', 'kittens');
+let db = new Meow('localhost', '5432', 'kittens');
 db.init();
-db.getSchema();
+db.createTables();
--- meow.js
+++ meow.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
+const pg = require('pg');
 
 function Meow(hostname, port, database) {
   this.hostname = hostname;
   this.port = port;
   this.database = database;
   this.schema = null;
+  this.client = null;
 }
 
 Meow.prototype.init = function(schema) {
@@ -20,7 +22,31 @@ Meow.prototype.getSchema = function() {
 }
 
 Meow.prototype.createTables = function() {
-  
+  //let connectionString = 'postgres://' + this.hostname + ':' + this.port + '/' + this.database;
+  //this.client = new pg.Client(connectionString);
+  //this.client.connect();
+
+  let schema = this.schema;
+  Object.keys(schema).map(function(tableKey) {
+    let queryString = 'CREATE TABLE ' + tableKey + '(';
+
+    Object.keys(schema[tableKey]).map(function(fieldKey, fieldIndex) {
+      let fieldQuery = fieldKey + ' ' + schema[tableKey][fieldKey];
+
+      // Do not add comma after last item.
+      if (fieldIndex < (Object.keys(schema[tableKey]).length - 1)) {
+        fieldQuery += ', ';
+      }
+      queryString += fieldQuery;
+    });
+
+    queryString += ')'
+    console.log(queryString);
+  });
+
+  //let queryString = 'CREATE TABLE ';
+  //let createDatabase = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+  //query.on('end', () => { client.end(); });
 }
 
 module.exports = Meow;
