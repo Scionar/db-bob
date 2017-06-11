53ef60047792b5460378933500b29d052317e104
Joona Viertola <contact@joonaviertola.com>
2017-06-11 20:28:32 +0300

    Check that schema file exists. And output schema.

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ const Meow = require("./meow.js");
 
 let db = new Meow('localhost', '3322', 'kittens');
 db.init();
+db.getSchema();
--- meow.js
+++ meow.js
@@ -1,11 +1,26 @@
+const fs = require('fs');
+
 function Meow(hostname, port, database) {
   this.hostname = hostname;
   this.port = port;
   this.database = database;
+  this.schema = null;
 }
 
-Meow.prototype.init = function() {
-  console.log(this.hostname + ':' + this.port + '/' + this.database);
+Meow.prototype.init = function(schema) {
+  let schema_url = (schema !== undefined && typeof schema === 'string' ) ? schema : 'schema.json';
+
+  if (fs.existsSync(schema_url)) {
+    this.schema = JSON.parse(fs.readFileSync(schema_url, 'utf8'));
+  }
+}
+
+Meow.prototype.getSchema = function() {
+  console.log(this.schema);
+}
+
+Meow.prototype.createTables = function() {
+  
 }
 
 module.exports = Meow;
